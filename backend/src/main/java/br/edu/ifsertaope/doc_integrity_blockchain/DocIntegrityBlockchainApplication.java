066353d3dbb6f57bdd514c90068df19e70d2db01
Gabriel Leal <gabriel-o-carinha@hotmail.com>
2026-04-19 19:42:33 -0300

    feat: add Spring Boot project

---
+++ backend/src/main/java/br/edu/ifsertaope/doc_integrity_blockchain/DocIntegrityBlockchainApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifsertaope.doc_integrity_blockchain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocIntegrityBlockchainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocIntegrityBlockchainApplication.class, args);
+	}
+
+}
