8ec3ceb862489928b801da0ecf78b9146edf2d44
Gabriel Leal <gabriel-o-carinha@hotmail.com>
2026-04-18 21:25:27 -0300

    feat: add hardhat project

---
+++ blockchain/hardhat.config.ts
@@ -0,0 +1,31 @@
+import hardhatToolboxMochaEthersPlugin from "@nomicfoundation/hardhat-toolbox-mocha-ethers";
+import { configVariable, defineConfig } from "hardhat/config";
+
+export default defineConfig({
+  plugins: [hardhatToolboxMochaEthersPlugin],
+  solidity: {
+    profiles: {
+      default: {
+        version: "0.8.28",
+      },
+      production: {
+        version: "0.8.28",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
+    },
+  },
+  networks: {
+    amoy: {
+      type: "http",
+      chainType: "l1",
+      url: configVariable("AMOY_RPC_URL"),
+      accounts: [configVariable("PRIVATE_KEY")],
+      chainId: 80002,
+    },
+  },
+});
